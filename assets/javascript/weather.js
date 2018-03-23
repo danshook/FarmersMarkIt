8e95b35a63103d26d75a2f4efb2be1f8b4a47879
Alexander Bly <alexanderbly@Alexanders-MacBook-Pro.local>
2018-03-22 21:26:29 -0700

    merging master

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 
-	var currentWeather;
-
+	//var currentWeather;
+	console.log("test")
 	$.ajax({
-
 		url: 'http://api.openweathermap.org/data/2.5/weather?q=Phoenix,us&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
-	}).then(function(weatherObj) {
-		currentWeather = weatherObj,
-		console.log(currentWeather),
-		}
+		}).then(function(response){
+			console.log(response);
+		})
+
+
+
+	//}).then(function(weatherObj) {
+		
 });
