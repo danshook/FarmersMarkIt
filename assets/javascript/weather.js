2c97f7f7568b6306f8d22a4cf34bf6042cc6aee3
Alexander Bly <alexanderbly@Alexanders-MacBook-Pro.local>
2018-03-24 11:15:56 -0700

    weather API changes and CSS

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 
 	$.ajax({
-		url: 'http://api.openweathermap.org/data/2.5/weather?q=Phoenix,us&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
+		url: 'http://api.openweathermap.org/data/2.5/weather?id=5308655&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
@@ -15,14 +15,46 @@ $(document).ready(function(){
 		})
 
 	$.ajax({
-		url: 'http://http://api.openweathermap.org/data/2.5/weather?q=Tempe,us&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
+		url: 'http://api.openweathermap.org/data/2.5/weather?id=5317058&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
-			var tempeWeatherId = "Today's weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			var tempeWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
 			
-			$("#tempeWeather").text(tempeWeatherId);
+			$(".tempeWeather").text(tempeWeatherId);
 			
 		})
 
+		$.ajax({
+		url: 'http://api.openweathermap.org/data/2.5/weather?id=5295903&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
+		type: "GET"
+		}).then(function(response){
+			
+			var gilbertWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			
+			$(".gilbertWeather").text(gilbertWeatherId);
+			
+		})	
+
+		$.ajax({
+		url: 'http://api.openweathermap.org/data/2.5/weather?id=5308655&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
+		type: "GET"
+		}).then(function(response){
+			
+			var ahwatukeeWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			
+			$(".ahwatukeeWeather").text(ahwatukeeWeatherId);
+			
+		})
+			
+		$.ajax({
+		url: 'http://api.openweathermap.org/data/2.5/weather?q=Scottsdale,us&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
+		type: "GET"
+		}).then(function(response){
+			
+			var scottsdaleWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			
+			$(".scottsdaleWeather").text(scottsdaleWeatherId);
+			
+		})	
 });
