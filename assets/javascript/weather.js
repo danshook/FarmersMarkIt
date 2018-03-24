82e393968568adbb74d714accd769140d8c26bec
Alexander Bly <alexanderbly@Alexanders-MacBook-Pro.local>
2018-03-24 11:41:29 -0700

    changes to weather.js

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-
-
+	//AJAX request and writing to page for general Phx weather in upper right corner
 	$.ajax({
 		url: 'http://api.openweathermap.org/data/2.5/weather?id=5308655&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
@@ -14,47 +13,53 @@ $(document).ready(function(){
 			
 		})
 
+	//AJAX request and writing to page for Tempe weather in market card
 	$.ajax({
 		url: 'http://api.openweathermap.org/data/2.5/weather?id=5317058&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
-			var tempeWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			var tempeWeatherId = response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
 			
 			$(".tempeWeather").text(tempeWeatherId);
 			
 		})
 
+		//AJAX request and writing to page for Gilbert weather in market card
 		$.ajax({
 		url: 'http://api.openweathermap.org/data/2.5/weather?id=5295903&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
-			var gilbertWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			var gilbertWeatherId = response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
 			
 			$(".gilbertWeather").text(gilbertWeatherId);
 			
 		})	
 
+		//AJAX request and writing to page for Ahwatukee weather in market card	
 		$.ajax({
 		url: 'http://api.openweathermap.org/data/2.5/weather?id=5308655&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
-			var ahwatukeeWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			var ahwatukeeWeatherId = response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
 			
 			$(".ahwatukeeWeather").text(ahwatukeeWeatherId);
 			
 		})
-			
+		
+		//AJAX request and writing to page for Scottsdale weather in market card	
 		$.ajax({
 		url: 'http://api.openweathermap.org/data/2.5/weather?q=Scottsdale,us&appid=028ca2a1c72ae0a27b9432b8005fae88&units=imperial',
 		type: "GET"
 		}).then(function(response){
 			
-			var scottsdaleWeatherId = "Weather: " + response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
+			var scottsdaleWeatherId = response.weather[0].description + " | " + parseInt(response.main.temp_min) + "°" + " / " + parseInt(response.main.temp_max) + "°"
 			
 			$(".scottsdaleWeather").text(scottsdaleWeatherId);
 			
 		})	
+
+
 });
